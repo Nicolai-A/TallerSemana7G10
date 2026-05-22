2bc6d517d7c0051ace834b2ab456154281753fc3
Nicolas <emilio.acosta@udla.edu.ec>
2026-05-21 21:35:51 -0500

    ACTUALIZADO

--- src/Ventana.java
+++ src/Ventana.java
@@ -1,5 +1,228 @@
+import controlador.GestionAvengers;
+import modelo.Avenger;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Ventana {
     private JPanel ventana;
+    private JTabbedPane tabbedPane1;
+    private JTextField txtNombreRegistro;
+    private JTextField txtMisionRegistro;
+    private JTextField txtPagoRegistro;
+    private JButton btnRegistrar;
+    private JTextField txtNombreBuscar;
+    private JTextField txtMisionBuscar;
+    private JTextField txtPagoBuscar;
+    private JButton btnBuscar;
+    private JButton btnModificar;
+    private JSpinner spnPeligrosidadBuscar;
+    private JSpinner SpnIdRegistro;
+    private JSpinner spnPeligrosidadRegistro;
+    private JSpinner spnBuscarId;
+    private JSpinner spnReporteId;
+    private JTextArea txtAreaReporte;
+    private JButton btnReporteId;
+    private JScrollBar scrollBar1;
+    private GestionAvengers gestion;
+
+    public Ventana() {
+
+        gestion = new GestionAvengers();
+
+
+        btnRegistrar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                try {
+
+                    int id = (int) SpnIdRegistro.getValue();
+
+                    String nombre = txtNombreRegistro.getText();
+
+                    String mision = txtMisionRegistro.getText();
+
+                    int peligrosidad = (int) spnPeligrosidadRegistro.getValue();
+
+                    double pago =
+                            Double.parseDouble(txtPagoRegistro.getText());
+
+                    if (nombre.isEmpty() || mision.isEmpty()) {
+
+                        JOptionPane.showMessageDialog(null, "Complete todos los campos");
+
+                        return;
+                    }
+
+                    Avenger avenger = new Avenger(
+                            id,
+                            nombre,
+                            mision,
+                            peligrosidad,
+                            pago
+                    );
+
+                    boolean agregado = gestion.agregarAvenger(avenger);
+
+                    if (agregado) {
+
+                        JOptionPane.showMessageDialog(null, "Avenger registrado correctamente");
+
+                        limpiarRegistro();
+
+                    } else {
+                        JOptionPane.showMessageDialog(null, "El ID ya existe");
+                    }
+
+                } catch (Exception ex) {
+                    JOptionPane.showMessageDialog(null, "Error en los datos");
+                }
+            }
+        });
+
+        btnBuscar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                try {
+
+                    int id = (int) spnBuscarId.getValue();
+
+                    int indice = gestion.buscarPorId(id);
+
+                    if (indice != -1) {
+
+                        Avenger avenger = gestion.getValor(indice);
+
+                        txtNombreBuscar.setText(avenger.getNombre());
+
+                        txtMisionBuscar.setText(avenger.getMision());
+
+                        spnPeligrosidadBuscar.setValue(avenger.getPeligrosidad());
+
+                        txtPagoBuscar.setText(String.valueOf(avenger.getPagoMensual()));
+
+                    } else {
+
+                        JOptionPane.showMessageDialog(null, "Avenger no encontrado");
+                    }
+
+                } catch (Exception ex) {
+
+                    JOptionPane.showMessageDialog(null, ex.getMessage());
+                }
+            }
+        });
+        btnModificar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                try {
+
+                    int id = (int) spnBuscarId.getValue();
+
+                    String nombre = txtNombreBuscar.getText();
+
+                    String mision = txtMisionBuscar.getText();
+
+                    int peligrosidad = (int) spnPeligrosidadBuscar.getValue();
+
+                    double pago = Double.parseDouble(txtPagoBuscar.getText());
+
+                    boolean modificado =
+                            gestion.modificarAvenger(
+                                    id,
+                                    nombre,
+                                    mision,
+                                    peligrosidad,
+                                    pago
+                            );
+
+                    if (modificado) {
+
+                        JOptionPane.showMessageDialog(null, "Avenger modificado correctamente");
+
+                    } else {
+
+                        JOptionPane.showMessageDialog(null, "No se pudo modificar");
+                    }
+
+                } catch (Exception ex) {
+
+                    JOptionPane.showMessageDialog(null, "Error al modificar");
+                }
+            }
+        });
+
+        btnReporteId.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                try {
+
+                    int id = (int) spnReporteId.getValue();
+
+                    int indice = gestion.buscarPorId(id);
+
+                    if (indice != -1) {
+
+                        Avenger avenger = gestion.getValor(indice);
+
+                        txtAreaReporte.setText("");
+
+                        txtAreaReporte.append(
+                                avenger.toString()
+                                        + "\n\n"
+                                        + "Fondo de Heroes: $"
+                                        + avenger.calcularFondoHeroes()
+                                        + "\n\n"
+                                        + "Impuesto Gobierno: $"
+                                        + avenger.calcularImpuesto()
+                                        + "\n\n"
+                                        + "Pago Neto: $"
+                                        + avenger.calcularPagoNeto()
+                        );
+
+                    } else {
+
+                        JOptionPane.showMessageDialog(null, "Avenger no encontrado");
+                    }
+
+                } catch (Exception ex) {
+
+                    JOptionPane.showMessageDialog(null, ex.getMessage());
+                }
+            }
+        });
+
+    }
+    private void limpiarRegistro() {
+
+        txtNombreRegistro.setText("");
+        txtMisionRegistro.setText("");
+        txtPagoRegistro.setText("");
+
+        SpnIdRegistro.setValue(1);
+        spnPeligrosidadRegistro.setValue(1);
+    }
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Sistema Avengers");
+
+        frame.setContentPane(new Ventana().ventana);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.pack();
+
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
+    }
+    public JPanel getVentana() {
+
+        return ventana;
+    }
 }
--- src/controlador/GestionAvengers.java
+++ src/controlador/GestionAvengers.java
@@ -1,53 +1,114 @@
 package controlador;
 import modelo.Avenger;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class GestionAvengers {
+
     private ArrayList<Avenger> listaAvengers;
 
     public GestionAvengers() {
+
         listaAvengers = new ArrayList<>();
     }
 
+
     public boolean agregarAvenger(Avenger avenger) {
 
-        if (buscarPorId(avenger.getId()) == null) {
+        if (buscarPorId(avenger.getId()) == -1) {
 
             listaAvengers.add(avenger);
+
+            ordenarPorId();
+
             return true;
         }
 
         return false;
     }
 
+
+    private void ordenarPorId() {
+
+        Collections.sort(listaAvengers, Comparator.comparingInt(Avenger::getId));
+    }
+
+
     public ArrayList<Avenger> listarAvengers() {
+
         return listaAvengers;
     }
 
-    public Avenger buscarPorId(int id) {
 
-        for (Avenger a : listaAvengers) {
+    public int buscarPorId(int id) {
 
-            if (a.getId() == id) {
-                return a;
-            }
+        if (listaAvengers.isEmpty() || id < listaAvengers.get(0).getId() || id > listaAvengers.get(listaAvengers.size() - 1).getId()) {
+
+            return -1;
         }
 
-        return null;
+        return buscarBinarioRecursivo(id, 0, listaAvengers.size() - 1);
+    }
+
+    private int buscarBinarioRecursivo(
+            int id,
+            int inferior,
+            int superior
+    ) {
+
+        if (inferior > superior) {
+
+            return -1;
+        }
+
+        int centro = (inferior + superior) / 2;
+
+        if (listaAvengers.get(centro).getId() == id) {
+
+            return centro;
+
+        } else if (listaAvengers.get(centro).getId() > id) {
+
+            return buscarBinarioRecursivo(id, inferior, centro - 1);
+
+        } else {
+
+            return buscarBinarioRecursivo(id, centro + 1, superior);
+        }
+    }
+
+    public Avenger getValor(int indice) throws Exception {
+
+        if (indice < 0 || indice >= listaAvengers.size()) {
+
+            throw new Exception("Indice fuera de rango");
+        }
+
+        return listaAvengers.get(indice);
     }
 
     public boolean modificarAvenger(int id, String nombre, String mision, int peligrosidad, double pagoMensual) {
 
-        Avenger avenger = buscarPorId(id);
+        int indice = buscarPorId(id);
 
-        if (avenger != null) {
+        if (indice != -1) {
 
-            avenger.setNombre(nombre);
-            avenger.setMision(mision);
-            avenger.setPeligrosidad(peligrosidad);
-            avenger.setPagoMensual(pagoMensual);
+            try {
 
-            return true;
+                Avenger avenger = getValor(indice);
+
+                avenger.setNombre(nombre);
+                avenger.setMision(mision);
+                avenger.setPeligrosidad(peligrosidad);
+                avenger.setPagoMensual(pagoMensual);
+
+                return true;
+
+            } catch (Exception e) {
+
+                return false;
+            }
         }
 
         return false;
